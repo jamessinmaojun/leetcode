fd7599fb2f6cdf0417ff7f1c2013604b927a517d
theGiantPeach <122222262+jamessinmaojun@users.noreply.github.com>
2026-04-02 00:36:18 +0800

    feat(217): add solution

---
+++ 217 - Contains Duplicate/Solution.py
@@ -0,0 +1,16 @@
+"""
+Solution for LeetCode #217 - 'Contains Duplicate'
+
+Time Complexity:  O(n)
+Space Complexity: O(n)
+"""
+class Solution:
+  def containsDuplicate(self, nums: List[int]) -> bool:
+    s = set()
+    
+    for n in nums:
+      if n in s:
+        return True
+      s.add(n)
+
+    return False
