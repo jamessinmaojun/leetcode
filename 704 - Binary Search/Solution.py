956e643df184d359f647da754ee4f7ef511c94b3
theGiantPeach <122222262+jamessinmaojun@users.noreply.github.com>
2026-04-07 01:04:57 +0800

    feat(704): add solution

---
+++ 704 - Binary Search/Solution.py
@@ -0,0 +1,22 @@
+"""
+Solution for LeetCode #704 - 'Binary Search'
+
+Time Complexity:  O(nlogn)
+Space Complexity: O(1)
+"""
+class Solution:
+  def search(self, nums: List[int], target: int) -> int:
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+      m = l + ((r - l) // 2)
+      curr = nums[m]
+
+      if curr < target:
+        l = m + 1
+      elif curr > target:
+        r = m - 1
+      else:
+        return m
+
+    return - 1
