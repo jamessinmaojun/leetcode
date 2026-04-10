d8bb8cea68bc5aceefcde082fb8ec8dbbc276beb
theGiantPeach <122222262+jamessinmaojun@users.noreply.github.com>
2026-04-11 01:26:49 +0800

    feat(141): add solution

---
+++ 141 - Linked List Cycle/Solution.py
@@ -0,0 +1,25 @@
+"""
+Solution for LeetCode #141 - 'Linked List Cycle'
+
+Time Complexity:  O(n)
+Space Complexity: O(1)
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = nex
+
+class Solution:
+  def reverseList(self, head: Optional[ListNode]) -> bool:
+    slow, fast = head, head
+
+    while fast and fast.next:
+      slow = slow.next
+      fast = fast.next.next
+
+      if slow == fast:
+        return True
+
+    return False
